450294bd2b7536bf00f0d06e4add56965851304b
son loku <jacobrborgen@gmail.com>
2020-02-06 14:00:52 -0600

    added functional displays when each button is clicked

--- brains.js
+++ brains.js
@@ -1,1 +1,18 @@
-
+//when about button is clicked display info
+$("#about").on("click", function() {
+    $("#projectDisp").hide(1000);
+    $("#contactDisp").hide(1000);
+    $("#aboutDisp").toggle(1000);
+});
+//when projects button is clicked display projects
+$("#projects").on("click", function() {
+    $("#aboutDisp").hide(1000);
+    $("#contactDisp").hide(1000);
+    $("#projectDisp").toggle(1000);
+});
+//when contact button is clicked dipslay contact info
+$("#contact").on("click", function() {
+    $("#aboutDisp").hide(1000);
+    $("#projectDisp").hide(1000);
+    $("#contactDisp").toggle(1000);
+});
